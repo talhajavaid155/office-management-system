faeff41763fd4e5d00ed43dd6f6d860be90215ef
saadali7142 <saadali7142@gmail.com>
2022-07-07 11:39:00 +0500

    jwt code updated

--- backend/controllers/jwt.js
+++ backend/controllers/jwt.js
@@ -1,5 +1,8 @@
+const asyncHandler = require("express-async-handler");
+// const db = require("../db.config");
+const db = require("../models");
+const Employee = db.employee;
 const { sign, verify } = require("jsonwebtoken");
-
 const createTokens = (user) => {
   const payload = {
     username: user.userName,
@@ -12,20 +15,51 @@ const createTokens = (user) => {
 };
 
 //middleware (auth)
-const validateToken = (req, res, next) => {
-  const accessToken = req.cookies["access-token"];
-  if (!accessToken)
-    return res.status(400).json({ error: "User not Authenticated" });
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  // console.log(req.headers.authorization);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = verify(token, "talha");
+      console.log(
+        "🚀 ~ file: jwt.js ~ line 27 ~ validateToken ~ decoded",
+        decoded
+      );
 
-  try {
-    const validToken = verify(accessToken, "talha");
-    if (validToken) {
-      req.authenticated = true; // we can create variable by req.variable
-      return next(); // move frwrd to our req
+      // req.user = await Employee.findById(decoded.id).select('-password')
+      req.user = await Employee.findByPk(decoded.id);
+
+      // console.log(decoded);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not Authorized, token failed");
     }
-  } catch (err) {
-    return res.status(400).json({ error: err });
   }
-};
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+  next();
+  // const accessToken = req.cookies["access-token"];
+  // if (!accessToken)
+  //   return res.status(400).json({ error: "User not Authenticated" });
+
+  // try {
+  //   const validToken = verify(accessToken, "talha");
+  //   if (validToken) {
+  //     req.authenticated = true; // we can create variable by req.variable
+  //     return next(); // move frwrd to our req
+  //   }
+  // } catch (err) {
+  //   return res.status(400).json({ error: err });
+  // }
+});
 
 module.exports = { createTokens, validateToken };
